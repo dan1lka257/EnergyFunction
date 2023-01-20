9bbb3dfc447f945d923f59a4b17e1268c6de4a13
Eugene1BbMn <122548959+Eugene1BbMn@users.noreply.github.com>
2023-01-20 23:46:19 +0300

    add parser to receive numbers from terminal

--- draw_functions.py
+++ draw_functions.py
@@ -1,6 +1,35 @@
+import argparse
 import numpy as np
 from matplotlib import pyplot as plt
-n = int(input())
-domain = np.arange(-5, 5, 0.1)
-graph = plt.plot(domain, domain**n)
-plt.show()
+
+
+def argparser():
+    if __name__ == '__main__':
+        parser = argparse.ArgumentParser(
+            description="""
+
+            Welcome to Graphic Artist.
+
+            You can choose from four available graphs: x**n
+
+            -n is an integer degree parameter .
+
+            """
+        )
+
+        parser.add_argument(
+            "-n",
+            type=int,
+            default=2,
+            help=f"Natural number"
+        )
+
+        parsed_args = parser.parse_args()
+
+        return parsed_args
+def plot_builder(n):
+    domain = np.arange(-5, 5, 0.1)
+    graph = plt.plot(domain, domain ** n)
+    plt.show()
+
+plot_builder(*vars(argparser()).values())
