e51bf610502da3ae1fa0ca42170f8b58702a22a8
dan1lka257 <daniltursunov597@gmail.com>
2023-01-27 19:42:32 +0300

    fix error with digital <=1 & draw axes

--- draw_functions.py
+++ draw_functions.py
@@ -13,15 +13,25 @@ def print_function(n):
     ax = figure.add_subplot(1, 1, 1)
     lowerLimF = -5
     upperLimF = 5
-    x = np.arange(lowerLimF, upperLimF, 0.01)
+    x = np.arange(lowerLimF, upperLimF, 0.1)
     y = x ** n
     ax.plot(x, y)
-    ax.set_yticks(np.arange(min(0, lowerLimF**n), upperLimF**n + 1, upperLimF**n//10))
+    ax.set_yticks(np.arange(min(0, lowerLimF**n), upperLimF**n + 1, max(upperLimF**n//10, 1)))
     ax.set_xticks(np.arange(-5, 5 + 1, 1))
+    ax.plot([0, 0], [min(0, lowerLimF**n), upperLimF**n + 1], color="grey", linestyle="--")
+    ax.plot([lowerLimF, upperLimF], [0, 0], color="grey", linestyle="--")
     ax.grid(color="lightgray", linestyle=":")
     ax.set_title(f"y = x^{n} function", fontsize=20)
     ax.set_ylabel("Axe Y")
     ax.set_xlabel("Axe X")
     plt.show()
 
-print_function(argparser().number)
+def isCorrectNumber(num):
+    if num > 0:
+        print_function(num)
+    else:
+        print("Natural Numbers contains only positive integers such as 1, 2, 3, 4, 5, 6, and so on.")
+
+
+num = argparser().number
+isCorrectNumber(num)
