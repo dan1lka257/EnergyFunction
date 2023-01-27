9849dff3140ce2a442c874cfbbfb6ad2a5fa7276
dan1lka257 <daniltursunov597@gmail.com>
2023-01-27 22:21:57 +0300

    add |x - 1/2|^n in 2d & 3d (rounded) & edit manual

--- draw_functions.py
+++ draw_functions.py
@@ -9,18 +9,42 @@ def argparser():
     return args
 
 def print_function(n):
-    figure = plt.figure(num=2, figsize=(6, 6))
-    ax = figure.add_subplot(1, 1, 1)
-    lowerLimF = -5
-    upperLimF = 5
-    x = np.arange(lowerLimF, upperLimF, 0.1)
-    y = x ** n
-    ax.plot(x, y)
-    ax.spines['bottom'].set_position(('data',0))
-    ax.spines['left'].set_position(('data',0))
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.set_title(f'y = x^{n} function', fontsize=20)
+    # figure and axes
+    fig = plt.figure(num=2, figsize=(12, 6))
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+    radius = 1
+    # 2d axes
+    x1 = np.linspace(0, radius, 100)
+    y1 = abs(x1 - 1/2)**n
+    # parameter
+    t = 2 * np.pi * x1
+    # 3d axes
+    x2 = radius * np.cos(t)
+    y2 = radius * np.sin(t)
+    z2 = np.linspace(0, 0, 100)
+    z3 = abs(x1 - 1/2)**n
+    # print all of axes
+    ax1.plot(x1, y1, color='black')
+    ax2.plot(x2, y2, z2, color='black', alpha=0.5)
+    ax2.plot(x2, y2, z3, color='black')
+    # move coordinate axes into center
+    ax1.spines['bottom'].set_position(('data',0))
+    ax1.spines['left'].set_position(('data',0))
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    # text marks on the axes
+    ax1.set_xlabel('X', fontweight = 'bold', fontsize = 14)
+    ax1.set_ylabel('Y', fontweight = 'bold', fontsize = 14)
+    ax1.set_title(f'y = |x - 1/2|^{n} function', fontsize=16)
+    ax2.set_xlabel('X', fontweight = 'bold', fontsize = 14)
+    ax2.set_ylabel('Y', fontweight = 'bold', fontsize = 14)
+    ax2.set_zlabel('Z', fontweight = 'bold', fontsize = 14)
+    ax2.set_title(f'y = |x - 1/2|^{n} function into circle', fontsize=16)
+    # limiting 3d axes
+    ax2.set_xlim(-radius, radius)
+    ax2.set_ylim(-radius, radius)
+    ax2.set_zlim(0, max(z3))
     plt.show()
 
 def isCorrectNumber(num):
